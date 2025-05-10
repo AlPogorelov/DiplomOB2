e8c7f4045b5cebf971e6610ebfe034dd8347a93e
AlPogorelov <eczempl@gmail.com>
2025-05-10 18:53:04 +0600

    done?

--- config/settings.py
+++ config/settings.py
@@ -3,7 +3,10 @@ from datetime import timedelta
 from pathlib import Path
 from dotenv import load_dotenv
 
-load_dotenv(override=True)
+if os.path.exists(".env"):
+    from dotenv import load_dotenv
+    load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
